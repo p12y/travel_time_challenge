23c0c95cddbfddd5ce7be4684ae39a66515ba148
Peter Tyldesley <ptylde@gmail.com>
2017-07-20 20:23:21 +0100

    Add start date validation

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,6 +3,7 @@ class Journey < ApplicationRecord
 
   validates :name, presence: true
   validates :start_time, presence: true
+  validates :start_date, presence: true
 
   accepts_nested_attributes_for :meetings, reject_if: :all_blank, allow_destroy: true
 end
--- spec/models/journey_spec.rb
+++ spec/models/journey_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Journey, type: :model do
   it {should validate_presence_of :start_time}
+  it {should validate_presence_of :start_date}
   
   it {should have_many :meetings}
 end
