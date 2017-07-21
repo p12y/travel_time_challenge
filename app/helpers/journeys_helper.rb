e2303132f4d3961e6a7c906954577745c29050e6
Peter Tyldesley <ptylde@gmail.com>
2017-07-21 12:58:24 +0100

    Style index

--- app/helpers/journeys_helper.rb
+++ app/helpers/journeys_helper.rb
@@ -1,2 +1,11 @@
 module JourneysHelper
+  def journey_time(journey)
+    start_time = journey.start_time
+    end_time = journey.meetings.last.departure_time
+    distance_of_time_in_words(end_time - start_time)
+  end
+
+  def format_time(time)
+    time.strftime('%I:%M %p')
+  end
 end
