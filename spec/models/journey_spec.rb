b4114f1f17b662dffd1c7c74a49b1d6dfb843a62
Peter Tyldesley <ptylde@gmail.com>
2017-07-23 12:27:47 +0100

    Add model specs

--- spec/models/journey_spec.rb
+++ spec/models/journey_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 RSpec.describe Journey, type: :model do
   it {should validate_presence_of :start_time}
   it {should validate_presence_of :start_date}
-  
+  it {should validate_presence_of :meetings}
+  it {should validate_presence_of :name}
+
+  it {should accept_nested_attributes_for :meetings}
   it {should have_many :meetings}
+
+  it "deletes associated record on delete" do
+    journey = create(:journey_with_meetings)
+    journey.destroy
+    expect(Meeting.count).to eq 0
+  end
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -14,18 +14,19 @@ RSpec.describe Meeting, type: :model do
 
 
   it "finds next record" do
-    j = create(:journey_with_meetings)
+    create(:journey_with_meetings)
     expect(Meeting.first.next.id).to eq Meeting.second.id
   end
 
   it "finds previous record" do
-    j = create(:journey_with_meetings)
+    create(:journey_with_meetings)
     expect(Meeting.last.previous.id).to eq Meeting.first.id
   end
 
   it "generates array of durations" do
-
+    create(:journey_with_meetings)
+    expect(Meeting.first.durations.first).to eq ["00:00", 0]
+    expect(Meeting.first.durations.last).to eq ["23:55", 1435]
   end
 
-  
 end
