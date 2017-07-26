b3c5d7f37c39bd0744f470c09d91dd9bf5112a21
Peter Tyldesley <ptylde@gmail.com>
2017-07-26 10:43:07 +0100

    Change before_save to after_save to avoid assigning id

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,7 +7,7 @@ class Meeting < ApplicationRecord
   validates :name, presence: true
   validates :duration, presence: true
 
-  before_save :calculate_travel_time
+  after_save :calculate_travel_time
 
   def next
     self.journey.meetings.where("id > ?", self.id).first
@@ -29,13 +29,6 @@ class Meeting < ApplicationRecord
   private
 
     def calculate_travel_time
-      # Set id to allow next/previous methods to work if new record
-      if Meeting.count != 0
-        self.id = self.id || Meeting.last.id + 1
-        else
-        self.id = 1
-      end
-
       @start_coords = '51.5199586,-0.0984249' # Rentify coordinates
       @departure_time = self.journey.start_time
       @start_time = self.journey.start_time
@@ -58,13 +51,20 @@ class Meeting < ApplicationRecord
                   time: @start_time, time_type: 'arrival', 
                   key: '1a1fb831e477a3b580de248076775e2d' 
                 }
+                
       @uri.query = URI.encode_www_form(@params)
-      @res = Net::HTTP.get_response(@uri)
+
+      begin
+        @res = Net::HTTP.get_response(@uri)
+      rescue SocketError => e
+        self.journey.errors.add(:request, "something went wrong")
+        raise ActiveRecord::RecordInvalid.new(self)
+      end
 
       # Return if not a success
       if !@res.is_a?(Net::HTTPSuccess)
         self.journey.errors.add(:request, "something went wrong")
-        throw :abort
+        raise ActiveRecord::RecordInvalid.new(self)
       end
 
       # Parse request
@@ -73,15 +73,15 @@ class Meeting < ApplicationRecord
       # Return if API returns error
       if @json["error_message"]
         self.journey.errors.add(:request, @json["error_message"])
-        throw :abort
+        raise ActiveRecord::RecordInvalid.new(self)
       end
       
       # Set departure/arrial time and add meeting duration
       @travel_time = @json["travel_time_minutes"]
-      self.travel_time = @travel_time
-      self.arrival_time = @departure_time + self.travel_time.minutes
-      self.departure_time = self.arrival_time
-      self.departure_time += self.duration.minutes
+      self.update_column(:travel_time, @travel_time)
+      self.update_column(:arrival_time, @departure_time + self.travel_time.minutes)
+      self.update_column(:departure_time, self.arrival_time)
+      self.update_column(:departure_time, self.departure_time + self.duration.minutes)
     end
 
     def get_coord(code)
